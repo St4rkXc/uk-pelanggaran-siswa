357c925d3cbb04d3094ba87c1844a8c0acd5dba5
Stark <123723458+St4rkXc@users.noreply.github.com>
2026-02-18 17:17:20 +0800

    fix: fixing undetected session user id due to wrong var name

--- auth/login_process.php
+++ auth/login_process.php
@@ -11,18 +11,18 @@ $stmt->execute([$usernameInput]);
 $user = $stmt->fetch();
 
 if ($user && password_verify($passwordInput, $user['password'])) {
-
     $_SESSION['login'] = true;
-    $_SESSION['id']    = $user['id'];
+
+    // GANTI INI: Pakai nama kolom yang bener (id_users)
+    $_SESSION['id_users'] = $user['id_users'];
+
     $_SESSION['nama']  = $user['name'];
     $_SESSION['role']  = $user['role'];
     $_SESSION['id_siswa'] = $user['id_siswa'];
 
-
     header("Location: ../pages/dashboard/{$user['role']}.php");
     exit;
 }
-
 echo "Login gagal";
 
 if ($user) {
